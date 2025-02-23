efad5b2c94aa90340ac6660dc28fa0f17ffd20aa
immprada <im.mprada@gmail.com>
2025-02-23 01:42:35 -0500

    feat(auth): add callback_url to configuration

--- lib/accc/configuration.rb
+++ lib/accc/configuration.rb
@@ -1,6 +1,6 @@
 module ACCC
   class Configuration
-    attr_accessor :client_id, :client_secret, :base_url
+    attr_accessor :client_id, :client_secret, :callback_url, :base_url
 
     def initialize
       @base_url = 'https://developer.api.autodesk.com'
