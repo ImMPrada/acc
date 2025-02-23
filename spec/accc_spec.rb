492cc2926aecf215999ff7feb56209fac3656e5c
immprada <im.mprada@gmail.com>
2025-02-23 00:58:26 -0500

    style: Apply RuboCop fixes to test files

--- spec/accc_spec.rb
+++ spec/accc_spec.rb
@@ -1,17 +1,17 @@
 RSpec.describe ACCC do
-  it "has a version number" do
-    expect(ACCC::VERSION).not_to be nil
+  it 'has a version number' do
+    expect(ACCC::VERSION).not_to be_nil
   end
 
-  describe ".configure" do
-    it "allows setting configuration" do
-      api_key = "test_key"
-      
-      ACCC.configure do |config|
+  describe '.configure' do
+    it 'allows setting configuration' do
+      api_key = 'test_key'
+
+      described_class.configure do |config|
         config.api_key = api_key
       end
 
-      expect(ACCC.configuration.api_key).to eq(api_key)
+      expect(described_class.configuration.api_key).to eq(api_key)
     end
   end
-end 
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,18 @@
-require "bundler/setup"
-require "accc"
-require "webmock/rspec"
-require "vcr"
+require 'bundler/setup'
+require 'accc'
+require 'webmock/rspec'
+require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "spec/vcr_cassettes"
+  config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data("<API_KEY>") { ACCC.configuration&.api_key }
+  config.filter_sensitive_data('<API_KEY>') { ACCC.configuration&.api_key }
 end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
@@ -21,9 +21,9 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
-  config.before(:each) do
+  config.before do
     ACCC.configure do |c|
-      c.api_key = "test_api_key"
+      c.api_key = 'test_api_key'
     end
   end
-end 
+end
